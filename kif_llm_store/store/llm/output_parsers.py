39720a7914684754ddcf5e02f30c45ab2a1fc852
Marcelo <marcelo.oc.machado@gmail.com>
2024-09-30 01:34:29 -0300

    Add more LLM parser methods

--- kif_llm_store/store/llm/output_parsers.py
+++ kif_llm_store/store/llm/output_parsers.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2024 IBM Corp.
 # SPDX-License-Identifier: Apache-2.0
 
-import string
+from decimal import Decimal
 from typing import List, override
 from langchain_core.output_parsers import (
     BaseOutputParser,
@@ -15,33 +15,87 @@ from langchain_core.output_parsers import (
 )
 
 
-class CommaSeparatedListOutputParserCleaned(CommaSeparatedListOutputParser):
+class SemicolonSeparatedListOutputParser(CommaSeparatedListOutputParser):
     @override
     def parse(self, text: str) -> List[str]:
-        """Parse the output of an LLM call.
+        '''Parse the output of an LLM call.
 
         Args:
             text: The output of an LLM call.
 
         Returns:
             A list of strings.
-        """
+        '''
 
-        parts = text.split(",")
+        parts = text.split(";")
 
         # Remove punctuation from each slice
-        cleaned_parts = [
-            part.translate(str.maketrans('', '', string.punctuation)).strip()
-            for part in parts
-        ]
+        cleaned_parts = [part.rstrip('?!.;:').strip() for part in parts]
 
         return cleaned_parts
+
+    @override
+    def get_format_instructions(self) -> str:
+        '''Return the format instructions for the semicolon-separated list output.'''
+        return (
+            'Your response should be a list of semicolon separated values, '
+            'eg: `foo; bar; baz` or `foo;bar;baz`'
+        )
+
+
+class SemicolonSeparatedListOfNumbersOutputParser(
+    SemicolonSeparatedListOutputParser
+):
+    @override
+    def parse(self, text: str) -> List[Decimal]:
+        import re
+
+        '''Parse the output of an LLM call.
+
+        Args:
+            text: The output of an LLM call.
+
+        Returns:
+            A list of numbers.
+        '''
+
+        parts = text.split(";")
+
+        numbers = []
+
+        for part in parts:
+            if not part:
+                continue
+            cleaned_part = part.rstrip('?!.;:').strip()
+            cleaned_part = re.sub(r'[^\d\.\-eE]+', '', cleaned_part).replace(
+                ',', ''
+            )
+            try:
+                number = int(cleaned_part)
+            except ValueError:
+                try:
+                    number = Decimal(cleaned_part)
+                except ValueError:
+                    continue
+            numbers.append(number)
+
+        return numbers
+
+    @override
+    def get_format_instructions(self) -> str:
+        '''Return the format instructions for the semicolon-separated list output.'''
+        return (
+            'Your response should be a list of numeric values, separated by '
+            'semicolons. Ensure that decimal numbers use a dot (e.g., 3.14) '
+            'instead of a comma. Example: 100.5; 200; 3.14. '
+        )
 
 
 __all__ = (
     'BaseOutputParser',
     'CommaSeparatedListOutputParser',
-    'CommaSeparatedListOutputParserCleaned',
+    'SemicolonSeparatedListOutputParser',
+    'SemicolonSeparatedListOfNumbersOutputParser',
     'StrOutputParser',
     'SimpleJsonOutputParser',
     'MarkdownListOutputParser',
