8c32c0ab2ab1950f4ac2a6f74d0ec1ec1c504551
Marcelo <marcelo.oc.machado@gmail.com>
2024-10-09 12:01:52 -0300

    compiler: Fix imports

---
+++ kif_llm_store/store/llm/compiler/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2024 IBM Corp.
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from .llm.filter_compiler import LLM_FilterCompiler
+
+__all__ = ('LLM_FilterCompiler',)
---
+++ kif_llm_store/store/llm/compiler/llm/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2024 IBM Corp.
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from .filter_compiler import LLM_FilterCompiler, Variable, LogicalComponent
+
+__all__ = ('LLM_FilterCompiler', 'LogicalComponent', 'Variable')
